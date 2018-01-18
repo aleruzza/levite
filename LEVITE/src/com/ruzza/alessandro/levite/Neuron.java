a4374150a60c40cb228f3dac1334d44064836d11
Alessandro Ruzza <alessandro.ruzza99@gmail.com>
2018-01-18 22:37:04 +0100

    Neuron class and sys file

---
+++ LEVITE/src/com/ruzza/alessandro/levite/Neuron.java
@@ -0,0 +1,47 @@
+package com.ruzza.alessandro.levite;
+
+import java.util.ArrayList;
+
+public class Neuron {
+
+	private float soglia_attivazione;
+	private ArrayList<Float> pesi;
+	
+	public Neuron(ArrayList<Float> gene)
+	{
+		//la soglia di attivazione è il primo valore del gene
+		this.soglia_attivazione = gene.get(0);
+		gene.remove(0);
+		this.pesi = gene;
+		
+	}
+	
+	public float activate(ArrayList<Float> input)
+	{
+		float sum=0,ris;
+		//sommo i pesi ottenuti
+		if(input.size() != pesi.size())
+		{
+			System.out.println("[ERROR] -> numero di collegamenti utilizzati diverso da quello previsto");
+		}
+		for(int i=0;i<input.size();i++)
+		{
+			sum+=input.get(i)*pesi.get(i);
+		}
+		ris = sum/input.size();
+		return funzioneDiAttivazione(ris);
+	}
+
+	public int getNDendriti()
+	{
+		return pesi.size();
+	}
+	
+	private float funzioneDiAttivazione(float inp)
+	{
+		//funzione di attivazione
+		//!/(1+e^-a(x+1/2))
+		float ris = (float) ((float) 1/(1+Math.pow(Math.E, -soglia_attivazione*(inp-0.5))));
+		return ris;
+	}
+}
