a5ccae2effd62344357ffe77bebaafe7973de805
Alessandro Ruzza <alessandro.ruzza99@gmail.com>
2018-02-08 22:18:39 +0100

    8.2.18

--- LEVITE/src/com/ruzza/alessandro/levite/Layer.java
+++ LEVITE/src/com/ruzza/alessandro/levite/Layer.java
@@ -8,7 +8,7 @@ public class Layer {
 	
 	public Layer(ArrayList<Float> gene, int nn)
 	{
-		neurons = new ArrayList<>();
+		neurons = new ArrayList<Neuron>();
 		ArrayList<Float> val = new ArrayList<>();
 		int d = gene.size()/nn;
 		for(int i=0;i<nn;i++)
@@ -18,8 +18,26 @@ public class Layer {
 			{
 				val.add(j, gene.get(i+j));
 			}
-			ArrayList<Float> cval = (ArrayList<Float>) val.clone();
-			neurons.add(new Neuron(cval));
+			neurons.add(new Neuron((ArrayList<Float>) val.clone()));
 		}
+	}
+	
+	public ArrayList<Float> activate(ArrayList<Float> input)
+	{
+		/*
+		 * Questa funzione riceve riceve in input i valori 
+		 * di uscita dei neuroni del Layer precedente. Vengono quindi 
+		 * dati "in pasto" alla funzione activate() di ciascun neurone.
+		 * I valori di ritorno vengono immagazzinati in un Array e
+		 *  ritornati dalla funzione, sarà poi la classe Rete che si
+		 *  occuperà di inviarli al Layer successivo.
+		 */
+		
+		ArrayList<Float> result = new ArrayList<>();
+		for(Neuron neuron: neurons)
+		{
+			result.add(neuron.activate(input));
+		}
+		return result;
 	}
 }
--- LEVITE/src/com/ruzza/alessandro/levite/Rete.java
+++ LEVITE/src/com/ruzza/alessandro/levite/Rete.java
@@ -3,7 +3,10 @@ package com.ruzza.alessandro.levite;
 import java.util.ArrayList;
 
 public class Rete {
-	private ArrayList<ArrayList<Neuron>> net;
+	private ArrayList<Layer> net;
+	private ArrayList<Float> DNA;
+	private ArrayList<Float> DNAf; 	//fenotipi, del "DNA" si conservano solo
+									//i valori "dominanti" ovvero, per design, quelli maggiori
 	
 	
 }
